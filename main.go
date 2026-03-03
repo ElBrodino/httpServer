bd2189018d38cd5dfbb11a4b2ea0ddc3c89c7e78
Frederic Poirier <uk_jobs@hotmail.com>
2026-03-03 09:21:54 -0500

    decoupling

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"net/http"
+	"log"
 )
 
 func main() {
-	const filepathRoot = "."
-	sMux := http.NewServeMux()
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: sMux,
+	if err := run(); err != nil {
+		log.Fatal(err)
 	}
-	sMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-	server.ListenAndServe()
 }
