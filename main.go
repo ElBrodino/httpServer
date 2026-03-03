5bdc43c3959083550d92a766dab090c76bcf6efb
Frederic Poirier <uk_jobs@hotmail.com>
2026-03-03 09:04:15 -0500

    Created temp file that reads from server

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
+	rootString := http.Dir("/")
 	sMux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: sMux,
 	}
+	fileHandler := http.FileServer(rootString)
+	sMux.Handle(string(rootString), fileHandler)
 	server.ListenAndServe()
 }
